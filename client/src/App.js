f2f99bae2b89926ed527b8a61062a0d13378b775
NecroCake-Ast <vladislav.tarakanov@bk.ru>
2022-02-07 18:19:51 +0400

    Load client app

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import AppRouter from "./components/AppRouter";
+import './App.css';
+import NavBar from "./components/NavBar";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <AppRouter />
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/utils/consts.js
@@ -0,0 +1,8 @@
+export const HOME_ROUTE = '/'
+export const LOGIN_ROUTE = '/login'
+export const REGISTRATION_ROUTE = '/registration'
+export const PROFILE_ROUTE = '/profile'
+export const DOCUMENT_ROUTE = '/document'
+export const COMPLETED_ROUTE = '/completed'
+export const TASKS_ROUTE = '/tasks'
+export const ERROR_ROUTE = '/error'
