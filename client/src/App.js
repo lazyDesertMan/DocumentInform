10a552e82a1d86f25ba1fc90d51957ce87ad9935
NecroCake-Ast <vladislav.tarakanov@bk.ru>
2022-02-08 21:33:09 +0400

    Added restore of authorization status

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import Cookies from 'js-cookie'
 import { BrowserRouter } from "react-router-dom";
+import { Context } from "."
 import AppRouter from "./components/AppRouter";
 import './App.css';
 import NavBar from "./components/NavBar";
 
-
 function App() {
+  const {user} = useContext(Context)
+  if (Cookies.get("usr") != null)
+    user.setIsAuth(true);
   return (
     <BrowserRouter>
       <NavBar />
