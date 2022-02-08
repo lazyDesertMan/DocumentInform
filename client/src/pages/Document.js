356c67552ce4f9d6b35e8468db6dc5bb90478086
NecroCake-Ast <vladislav.tarakanov@bk.ru>
2022-02-08 21:02:52 +0400

    Docs list loading update (part 2)

--- client/src/pages/Document.js
+++ client/src/pages/Document.js
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from "react";
 //import { useNavigate } from "react-router-dom";
 import { Button, Form, Stack } from "react-bootstrap";
 import {documents } from "../http/userAPI";
-import Doc from '../classes/Doc';
+import DocumentList from '../classes/DocumentList';
+import { observer } from "mobx-react-lite";
 
-async function getDoc() {
-    let docs = new Doc();
+let docs = new DocumentList();
+
+async function load() {
+    let docs = new DocumentList();
     try{
         let data = await documents()
-        docs.setDoc(data);
+        docs.list = data;
         return docs;
     } catch(e) {
         console.log(e);
@@ -17,14 +20,18 @@ async function getDoc() {
 }
 
 const Document = () => {
+    load().then(response => {
+        docs.list = response.list;
+    })
+
+    return(<DocumentListPlace/>)
+};
+
+const DocumentListPlace = observer(() => {
     //const navigate = useNavigate();
     const currDate = new Date().toLocaleDateString();
     const currTime = new Date().toLocaleTimeString();
-    let docs = null;
-    getDoc().then(response => {
-        console.log("Loaded"); 
-        docs = response.list;
-    })
+
     return(
         <>
             <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
@@ -42,7 +49,7 @@ const Document = () => {
                 <Form style={{width: '60%', marginTop: '1%'}}>
                     <div className="list-group list-group-flush border-bottom scrollarea">
                         {Array.from({ length: docs.list.length }).map((_, idx) => (
-                            <a href="#" className="list-group-item list-group-item-action py-3 lh-tight">
+                            <a key={docs.list[idx].id} href="#" className="list-group-item list-group-item-action py-3 lh-tight">
                                 <div className="d-flex w-100 align-items-center justify-content-between">
                                 <strong className="mb-1">{docs.list[idx].name}</strong>
                                 <small>{docs.list[idx].effectiveDate}</small>
@@ -55,5 +62,5 @@ const Document = () => {
             </div>
         </>
     );
-};
+});
 export default Document;
