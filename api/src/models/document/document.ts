627f9061bb63b8b3b3755983607ac81fc604fb5e
NecroCake-Ast <vladislav.tarakanov@bk.ru>
2022-02-10 16:11:18 +0400

    Add document initialization method

--- api/src/models/document/document.ts
+++ api/src/models/document/document.ts
@@ -4,6 +4,15 @@ class Document {
     public description:    string;  //!< Краткое описание документа
     public filePath:       string;  //!< Имя файла (с расширением)
     public effectiveDate:  Date;    //!< Дата вступления документа в силу
+
+    public init(id : number, name : string, description : string, filePath : string, effectiveDate : Date) : Document {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.filePath = filePath;
+        this.effectiveDate = effectiveDate;
+        return this;
+    }
 }
 
 export default Document;
