5010029ca207bd3e9a66db789052cd4982ad461f
NecroCake-Ast <vladislav.tarakanov@bk.ru>
2022-02-13 16:19:20 +0400

    Add resend task support

--- api/src/services/data/task/iTaskRepository.ts
+++ api/src/services/data/task/iTaskRepository.ts
@@ -2,12 +2,15 @@ import { CompleteFact } from "../../../models/task/completeFact";
 import { ITask } from "../../../models/task/iTask";
 
 interface ITaskRepository {
-    list(userID : number) : ITask[];
-    activeList(userID : number) : ITask[];
-    completeList(userID : number) : CompleteFact[];
     findByID(id : number) : ITask;
     add(tsk : ITask) : number;
     remove(id : number) : boolean;
+
+    userActiveTasks(userID : number) : ITask[];
+    userCompleteTasks(userID : number) : CompleteFact[];
+    positionActiveTasks(posID : number) : ITask[];
+    positionCompleteTasks(posID : number) : CompleteFact[];
+    
     setCompleted(taskID : number, userID : number) : void;
     allowedDocs(userID : number) : number[];
 }
