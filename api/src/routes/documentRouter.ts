b847c5d673e94e2749491e6845f6e7b9e1ce32b1
NecroCake-Ast <vladislav.tarakanov@bk.ru>
2022-02-13 22:36:50 +0400

    Added sending files from API (temporary from ./files)
    Файлы временно загружаются из папки build/files или любой другой папки 'files', расположенной в той же директории, что и app.js.
    Исправление запланировано после реализации загрузки файлов на сервер

--- api/src/routes/documentRouter.ts
+++ api/src/routes/documentRouter.ts
@@ -4,6 +4,9 @@ import UserController from '../controllers/userController';
 import Document from '../models/document/document';
 import { UserData } from '../models/userData';
 
+import * as fs from 'fs';
+import { dirname } from 'path';
+
 const documentRouter = express.Router();
 const documentController : DocumentController = new DocumentController();
 const userController : UserController = new UserController();
@@ -22,5 +25,25 @@ documentRouter.post("/add", async (req: express.Request, res: express.Response) 
     doc.effectiveDate = new Date(req.body.effectiveDate);
     res.send(documentController.add(doc).toString());
 });
+
+// TODO: Исправить адреса. Возможно стоит применять просто URL.
+//       Точно можно будет решить, когда будет готова загрузка файла на сервер
+documentRouter.get("/read",async (req: express.Request, res: express.Response) => {
+    let user : UserData = await userController.getActiveUser(req);
+    let allowedDocs = await documentController.userDocs(user);
+    let docID = Number(req.query.docID);
+    let fileIdx : number = 0;
+    while (fileIdx < allowedDocs.length && allowedDocs[fileIdx].id != docID)
+        fileIdx++;
+    if (fileIdx != allowedDocs.length) {
+        let path = require.main.path + "/files/" + allowedDocs[fileIdx].filePath;
+        fs.readFile(path, function (err, data) {
+            res.contentType("application/pdf");
+            res.send(data);
+        });
+    }
+    else
+        res.send("");
+})
 
 export default documentRouter;
