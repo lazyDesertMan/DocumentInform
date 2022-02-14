77e03a52dcbfbc25da474125620eab983f74c3b7
AminASTU <irgaliev01@mail.ru>
2022-02-14 18:35:43 +0400

    pdf

--- client/src/pages/PDF.js
+++ client/src/pages/PDF.js
@@ -1,17 +1,37 @@
 import React, {useState} from "react";
 import {Document, Page} from "react-pdf";
+import { useParams } from "react-router";
+import {GetPDF} from "../http/userAPI"
 
+let application;
+async function load() {
+    try{
+        let data = await GetPDF()
+        return data;
+    } catch(e) {
+        console.log(e);
+        return null;
+    }
+}
 const PDF = () => {
+    load().then(response => {
+        application = response;
+    })
+
+    return(<DocDotPDF/>)
+};
+
+const DocDotPDF = () => {
+    const {id} = useParams();
     const [numPages, setNumPages] = useState(null);
     const [pageNumber, setPageNumber] = useState(1);
-
     function onDocumentLoadSuccess({ numPages }) {
         setNumPages(numPages);
     }
     return(
         <div>
             <Document
-                file="ИнтерфейсыЛАБ1.pdf"
+                file=""
                 onLoadSuccess={onDocumentLoadSuccess}
             >
                 <Page pageNumber={pageNumber} />
