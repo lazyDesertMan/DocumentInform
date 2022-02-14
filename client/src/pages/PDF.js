2173aa7ce5b6c2ccbed11c44da76dee3627882f1
AminASTU <irgaliev01@mail.ru>
2022-02-14 23:43:58 +0400

    add open pdf

--- client/src/pages/PDF.js
+++ client/src/pages/PDF.js
@@ -1,43 +1,26 @@
-import React, {useState} from "react";
-import {Document, Page} from "react-pdf";
-import { useParams } from "react-router";
-import {GetPDF} from "../http/userAPI"
+import React from "react";
+import { Button} from "react-bootstrap";
+import {DOCUMENT_ROUTE } from "../utils/consts";
+import '../css/style.css';
+import { useNavigate, useParams } from "react-router-dom";
 
-let application;
-async function load() {
-    try{
-        let data = await GetPDF()
-        return data;
-    } catch(e) {
-        console.log(e);
-        return null;
-    }
-}
 const PDF = () => {
-    load().then(response => {
-        application = response;
-    })
-
-    return(<DocDotPDF/>)
-};
-
-const DocDotPDF = () => {
     const {id} = useParams();
-    const [numPages, setNumPages] = useState(null);
-    const [pageNumber, setPageNumber] = useState(1);
-    function onDocumentLoadSuccess({ numPages }) {
-        setNumPages(numPages);
-    }
+    const navigate = useNavigate();
     return(
-        <div>
-            <Document
-                file=""
-                onLoadSuccess={onDocumentLoadSuccess}
-            >
-                <Page pageNumber={pageNumber} />
-            </Document>
-            <p>Page {pageNumber} of {numPages}</p>
-        </div>
+        <>
+            <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                <iframe src={"http://localhost:1337/api/document/read?docID=" + id} width="100%" height="685em" title="documentDotPDF"></iframe>
+            </div>
+            <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                <Button 
+                    variant={"outline-dark"}
+                    onClick={() => navigate(DOCUMENT_ROUTE)}
+                >
+                Ознакомиться
+                </Button>
+            </div>
+        </>
     );
 };
 export default PDF;
